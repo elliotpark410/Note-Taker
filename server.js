b1c0bdcbd2062bfb795c2d0c30d8ac30947bfedf
Elliot Park <elliotpark410@gmail.com>
2022-02-01 02:02:42 -0800

    include PORT listener

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Dependencies required
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
-var app = express();
+const app = express();
 
 // Sets up the Express app to handle data parsing for PUTS and POSTS
 app.use(express.urlencoded({ extended: true }));
@@ -12,4 +13,20 @@ app.use(express.json());
 // process.env.PORT will allow any PORT that a user enters or 3000
 // e.g. if you run node index.js ,Node will use 3000
 // If you run PORT=4444 node index.js, Node will use process.env.PORT which equals to 4444 in this example
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+
+
+
+
+
+
+
+
+
+
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
