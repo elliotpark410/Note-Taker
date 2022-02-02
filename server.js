456e7f9e90ec313d69e25e792b796ecc30226aad
Elliot Park <elliotpark410@gmail.com>
2022-02-02 01:35:00 -0800

    create function to delete api/notes by id

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.get('/notes', (req, res) => {
 // GET route for notes data stored as stringified array in db.json 
 app.get('/api/notes', (req, res) => {
   console.log('notes get route successful');
+  // use fs.readFile to capture data in db.json
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     err ? console.error(err) : console.log(data)
       if (err) throw err;
@@ -50,6 +51,7 @@ app.get('/api/notes', (req, res) => {
     // Need to JSON.parse(data) because it was stored as a stringified array
     let savedNotes = JSON.parse(data);
     console.log(savedNotes);
+    // return as res.json 
     res.json(savedNotes);
   });
 }); 
@@ -58,27 +60,35 @@ app.get('/api/notes', (req, res) => {
 // POST route to store notes data as stringified array in db.json
 app.post('/api/notes', (req, res) => {
   console.log('notes post route successful');
+  // use fs.readFile to capture data in db.json
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     err ? console.error(err) : console.log(data)
       if (err) throw err;
-  
+    // (data) is content stored in db.json
+    // Need to JSON.parse(data) because it was stored as a stringified array
     let savedNotes = JSON.parse(data);
   // req.body object allows you to access data in a string or JSON object that user entered 
   // e.g. a user does a POST to http://localhost:3000/api/notes in Insomnia or browser and enters data in object format, I will be able to access that data
   let newNote = req.body;
+
+  // create a variable for uniqueId equal to savedNotes.length (array with objects) and convert it to a string
   let uniqueId = savedNotes.length.toString();
   console.log(uniqueId);
+
+  // create a id key for the newNote object and make it equal to uniqueId
   newNote.id = uniqueId;
   console.log(newNote);
-
+  
+  // push newNote (title, text, id) to savedNotes
   savedNotes.push(newNote);
   console.log(savedNotes);
 
+    // fs.writeFileSync wll synchonize / update the db.json file with the savedNotes
     fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes, null, 2), 'utf8', (err, data) => {
       err ? console.error(err) : console.log(data)
         if (err) throw err;
     });
-
+  // show response of savedNotes in JSON format 
   res.json(savedNotes);
   });
 
@@ -86,8 +96,33 @@ app.post('/api/notes', (req, res) => {
 
 
 
+// DELETE route to delete notes based off id entered in api
+app.delete('/api/notes/:id', function (req, res) {
+  fs.readFile('db/db.json', 'utf8', (err, data) => {
+    err ? console.error(err) : console.log(data)
+    if (err) throw err;
 
+      let savedNotes = JSON.parse(data);
+      let noteId = req.params.id;
+      let newNotesId = 0;
 
+      savedNotes = savedNotes.filter(currentNote => {
+          return currentNote.id != noteId;
+      });
+
+      for (currentNote of savedNotes) {
+          currentNote.id = newNotesId.toString();
+          newNotesId++;
+      }
+
+      fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes, null, 2), 'utf8', (err, data) => {
+        err ? console.error(err) : console.log(data)
+        if (err) throw err;
+      });
+
+      res.json(savedNotes);
+  });
+});
 
 
 
